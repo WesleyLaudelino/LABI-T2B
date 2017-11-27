c03408c8ee7b3caaa85172814a353059f9d5e90c
Wesley Silva <wesleylaudelino@outlook.com>
2017-11-27 20:02:45 -0300

    Teste de alteração

--- WebApp_Wls/Models/MUsica.cs
+++ WebApp_Wls/Models/MUsica.cs
@@ -12,6 +12,7 @@ namespace WebApp_Wls.Models
         [Required(ErrorMessage ="Título é obrigatório")]
         [MaxLength(400, ErrorMessage ="Máximo 400 caracteres")]
         public string Titulo { get; set; }
+
         public Categoria Categoria { get; set; }
         public TipoMidia TipoMidia { get; set; }
         public DateTime DataCadastro { get; set; }
